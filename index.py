eda48c5a085d7f11f377c0e2066f6b792ffa7fd3
samir3112 <samirparate3112@gmail.com>
2024-12-03 14:55:36 +0530

    Add new transaction feature

--- index.py
+++ index.py
@@ -1,9 +1,6 @@
 print("This is new repo created by SAM")
 
 print("This is new feature branch ")
-<<<<<<< HEAD
 print("SAM")
 print("Online Transaction")
-=======
 print("Calculate Data")
->>>>>>> main
